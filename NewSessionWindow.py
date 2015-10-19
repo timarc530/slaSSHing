df1228194dc3dd14f2df44b5d590436cccc18402
timarc530 <usparatrooper52@gmail.com>
2015-10-19 10:28:50 -0400

    added key functionality

--- NewSessionWindow.py
+++ NewSessionWindow.py
@@ -7,51 +7,83 @@ ID_PASSWORD_STRING = 2
 ID_SUBMIT_BUTTON = 3
 ID_SSH_STRING = 4
 ID_QUIT = 5
+ID_OPEN = 6
 
 
 class myPrompt(wx.Dialog):
 
     def __init__(self, parent, title):
-        super(myPrompt, self).__init__(parent, title=title, size=(500, 300))
-
+        super(myPrompt, self).__init__(parent, title=title, size=(500, 400))
+        wx.EVT_CLOSE(self, self.OnQuit)
         self.Centre()
         self.Show(True)
 
         # define panes and sizers
         panel = wx.Panel(self)
         panel.SetBackgroundColour('#4f5049')
-        hbox = wx.BoxSizer(wx.HORIZONTAL)
-        fgs = wx.FlexGridSizer(rows=4, cols=2, vgap=9, hgap=25)
+        hbox = wx.BoxSizer(wx.VERTICAL)
+        fgs_name_and_host = wx.FlexGridSizer(rows=8, cols=2, vgap=9, hgap=25)
+        fgs_key_file = wx.FlexGridSizer(rows=4, cols=2, vgap=5, hgap=10)
+        fgs_control_buttons = wx.FlexGridSizer(rows=1, cols=2, vgap=9, hgap=25)
 
         # add buttons and text boxes
-        name_label = wx.StaticText(panel, label='NAME:')
+        name_label = wx.StaticText(panel, label='NAME')
         self.name_string = wx.TextCtrl(panel, id=ID_NAME_STRING)
-        name_label.SetBackgroundColour('white')
 
         host_label = wx.StaticText(panel, label='HOST')
         self.host_string = wx.TextCtrl(panel, id=ID_HOST_STRING)
-        host_label.SetBackgroundColour('white')
 
         password_label = wx.StaticText(panel, label='PASSWORD')
         self.password_string = wx.TextCtrl(panel, id=ID_PASSWORD_STRING)
-        password_label.SetBackgroundColour('white')
+
+        self.key_button = wx.Button(panel, id=ID_OPEN, label='SELECT KEY', size=(70, 30))
+        self.Bind(wx.EVT_BUTTON, self.OnKeyOpen, id=ID_OPEN)
+        self.key_label = wx.StaticText(panel, label='path to key file')
+
+        self.rb_RSA = wx.RadioButton(panel, label='RSA', size=(10, 10), style=wx.RB_GROUP)
+        self.Bind(wx.EVT_RADIOBUTTON, self.OnKeyTypeSet, id=self.rb_RSA.GetId())
+        self.rb_DSA = wx.RadioButton(panel, label='DSA', size=(10, 10))
+        self.Bind(wx.EVT_RADIOBUTTON, self.OnKeyTypeSet, id=self.rb_DSA.GetId())
+        self.rb_ECDSA = wx.RadioButton(panel, label='ECDSA', size=(10, 10))
+        self.Bind(wx.EVT_RADIOBUTTON, self.OnKeyTypeSet, id=self.rb_ECDSA.GetId())
+        self.key_type = self.rb_RSA.GetLabelText()
 
         self.submit_button = wx.Button(panel, wx.ID_OK, label='SUBMIT', size=(70, 30))
         self.cancel_button = wx.Button(panel, id=ID_QUIT, label='CANCEL', size=(70, 30))
         self.Bind(wx.EVT_BUTTON, self.OnQuit, id=ID_QUIT)
 
         # add widgets to sizer and expand the text boxes
-        fgs.AddMany([(name_label, 1, wx.EXPAND), (self.name_string, 1, wx.EXPAND),
-                     (host_label, 1, wx.EXPAND), (self.host_string, 1, wx.EXPAND),
-                     (password_label, 1, wx.EXPAND), (self.password_string, 1, wx.EXPAND),
-                     self.submit_button, self.cancel_button ])
+        fgs_name_and_host.AddMany([(name_label, 1, wx.EXPAND), (self.name_string, 1, wx.EXPAND),
+                                   (host_label, 1, wx.EXPAND), (self.host_string, 1, wx.EXPAND),
+                                   (password_label, 1, wx.EXPAND), (self.password_string, 1, wx.EXPAND)])
+        fgs_key_file.AddMany([self.key_button, (self.key_label, 1, wx.EXPAND),
+                             (0, 0), (self.rb_RSA, 1, wx.EXPAND),
+                             (0, 0), (self.rb_DSA, 1, wx.EXPAND),
+                             (0, 0), (self.rb_ECDSA, 1, wx.EXPAND)])
+        fgs_control_buttons.AddMany([self.submit_button, self.cancel_button])
 
-        fgs.AddGrowableCol(1)
-        hbox.Add(fgs, proportion=1, flag=wx.ALL | wx.EXPAND, border=15)
+        fgs_name_and_host.AddGrowableCol(1)
+
+        hbox.Add(fgs_name_and_host, proportion=1, flag=wx.ALL | wx.EXPAND, border=15)
+        hbox.Add(fgs_key_file, proportion=1, flag=wx.ALL | wx.EXPAND, border=10)
+        hbox.Add(fgs_control_buttons, proportion=1, flag=wx.ALL | wx.EXPAND, border=20)
         panel.SetSizer(hbox)
 
+        # lame hack for getting it to draw correctly
         self.Maximize()
         self.Restore()
+
+    def OnKeyTypeSet(self, event):
+        self.key_type = event.EventObject.GetLabelText()
+
+    def OnKeyOpen(self, event):
+        fileDialog = wx.FileDialog(self, "Open key file", "", "", "PEM files (*.pem)|*.pem|Text files (*.txt)|*.txt",
+                                   wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
+
+        if fileDialog.ShowModal() == wx.ID_CANCEL:
+            fileDialog.Destroy()
+
+        self.key_label.SetLabelText(fileDialog.GetPath())
 
     def OnQuit(self, event):
         self.Destroy()
--- myGUI.py
+++ myGUI.py
@@ -25,7 +25,6 @@ class GUI(wx.Frame):
 
         # add buttons and text boxes
         self.ssh_string = wx.StaticText(panel, id=ID_SSH_STRING, label='SSH String goes here')
-        self.ssh_string.SetBackgroundColour('white')
         self.incoming = wx.TextCtrl(panel, id=ID_INCOMING_TEXT, style=wx.TE_MULTILINE)
         self.outgoing = wx.TextCtrl(panel, id=ID_OUTGOING_TEXT, style=wx.TE_MULTILINE)
 
@@ -38,7 +37,7 @@ class GUI(wx.Frame):
         # add widgets to sizer and expand the text boxes
         fgs.AddMany([(self.ssh_string, 1, wx.EXPAND), (0, 0),
                      (self.incoming, 1, wx.EXPAND), (self.outgoing, 1, wx.EXPAND),
-                    get_button, send_button])
+                     get_button, send_button])
         fgs.AddGrowableRow(1)
         fgs.AddGrowableCol(0)
         fgs.AddGrowableCol(1)
@@ -57,9 +56,9 @@ class GUI(wx.Frame):
         self.Bind(wx.EVT_MENU, self.OnNew, nitem)
         self.SetTitle('HACKATHON GUI')
 
+    @staticmethod
     def OnGet(self, event):
-        tx = 'new ssh string received'
-        self.ssh_string.SetLabelText(tx)
+        print 'send not implemented yet'
 
     def OnSend(self, event):
         tx = self.outgoing.GetValue()
@@ -70,12 +69,16 @@ class GUI(wx.Frame):
         print 'new session'
         chooser = NewSessionWindow.myPrompt(None, title='Choose Session Parameters')
         if chooser.ShowModal() == wx.ID_OK:
-            name_string = chooser.name_string
-            host_string = chooser.host_string
-            password_string = chooser.password_string
-            print name_string.GetValue() + '@' + host_string.GetValue()
-        else:
-            chooser.Destroy()
+            name_string = chooser.name_string.GetValue()
+            host_string = chooser.host_string.GetValue()
+            password_string = chooser.password_string.GetValue()
+            if name_string != '' and host_string != '':
+                self.ssh_string.SetLabelText(name_string + '@' + host_string)
+
+            key_path = chooser.key_label.GetLabelText()
+            print chooser.key_type + ' key selected'
+            print key_path
+        chooser.Destroy()
 
     def OnQuit(self, event):
         print '***end testing***'
